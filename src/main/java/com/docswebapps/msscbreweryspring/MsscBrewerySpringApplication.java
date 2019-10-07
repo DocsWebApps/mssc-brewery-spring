232897047dd1d201487410655fe01a96cea3850e
Dave.Collier <lardelbow@gmail.com>
2019-10-07 20:59:28 +0100

    Initial commit

---
+++ src/main/java/com/docswebapps/msscbreweryspring/MsscBrewerySpringApplication.java
@@ -0,0 +1,13 @@
+package com.docswebapps.msscbreweryspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBrewerySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBrewerySpringApplication.class, args);
+	}
+
+}
